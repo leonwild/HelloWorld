3e89f589baa2b32a6234349ed158a0eb91701211
n386942 <n386942@localhost>
2018-08-24 17:01:49 +0100

    hello world

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author n386942
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
